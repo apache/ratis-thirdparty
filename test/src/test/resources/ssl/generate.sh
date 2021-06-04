22a479af5c82fb7de19fe1ee6ac1e5bca634c8c8
Elek Márton <elek@apache.org>
2021-06-04 15:10:34 +0200

    Regenerate test certificates with longer expiry

--- test/src/test/resources/ssl/generate.sh
+++ test/src/test/resources/ssl/generate.sh
@@ -22,14 +22,14 @@ echo Generate CA key:
 openssl genrsa -passout pass:1111 -des3 -out ca.key 4096
 echo Generate CA certificate:
 # Generates ca.crt which is the trustCertCollectionFile
-openssl req -passin pass:1111 -new -x509 -days 365 -key ca.key -out ca.crt -subj "/CN=${SERVER_CN}"
+openssl req -passin pass:1111 -new -x509 -days 3650 -key ca.key -out ca.crt -subj "/CN=${SERVER_CN}"
 echo Generate server key:
 openssl genrsa -passout pass:1111 -des3 -out server.key 4096
 echo Generate server signing request:
 openssl req -passin pass:1111 -new -key server.key -out server.csr -subj "/CN=${SERVER_CN}"
 echo Self-signed server certificate:
 # Generates server.crt which is the certChainFile for the server
-openssl x509 -req -passin pass:1111 -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt 
+openssl x509 -req -passin pass:1111 -days 3650 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt 
 echo Remove passphrase from server key:
 openssl rsa -passin pass:1111 -in server.key -out server.key
 echo Generate client key
@@ -38,7 +38,7 @@ echo Generate client signing request:
 openssl req -passin pass:1111 -new -key client.key -out client.csr -subj "/CN=${CLIENT_CN}"
 echo Self-signed client certificate:
 # Generates client.crt which is the clientCertChainFile for the client (need for mutual TLS only)
-openssl x509 -passin pass:1111 -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client.crt
+openssl x509 -passin pass:1111 -req -days 3650 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client.crt
 echo Remove passphrase from client key:
 openssl rsa -passin pass:1111 -in client.key -out client.key
 echo Converting the private keys to X.509:
