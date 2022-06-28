792f0bd2e3780ccef546c0cb5a3a905822f7b721
tison <wander4096@gmail.com>
2022-06-28 11:12:15 +0800

    RATIS-1600. Add protobuf-java-util (#35)

---
+++ test/src/test/java/org/apache/ratis/thirdparty/demo/proto/ProtoUtilTest.java
@@ -0,0 +1,42 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ratis.thirdparty.demo.proto;
+
+import org.apache.ratis.thirdparty.com.google.protobuf.util.JsonFormat;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Unit test for ensure that protobuf-java-util is properly shaded as ratis thirdparty.
+ */
+@RunWith(JUnit4.class)
+public class ProtoUtilTest {
+
+    @Test
+    public void testJsonFormatPrinter() throws Exception {
+        final HelloRequest request = HelloRequest.newBuilder().setName("ratis").build();
+        final String actual = JsonFormat.printer().print(request);
+        final String expected = "{\n" +
+            "  \"name\": \"ratis\"\n" +
+            "}";
+        Assert.assertEquals(expected, actual);
+    }
+
+}
