4cad915eb3e1ab661f0dbb59b9a683fe951ebf3e
Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>
2026-07-02 14:28:15 +0200

    RATIS-2579. Split CI build (#134)
    (cherry picked from commit 799471243f5df2623b9eeb79b12438e7626521de)

--- dev-support/checks/build.sh
+++ dev-support/checks/build.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
@@ -13,29 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This workflow defines events and conditions to execute CI.
+set -u -o pipefail
 
-name: ci
+CHECK="$( basename "${BASH_SOURCE[0]}" | cut -f1 -d'.' )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-on:
-  push:
-    branches-ignore:
-      - 'dependabot/**'
-    tags:
-      - '**'
-  pull_request:
+MAVEN_OPTIONS='-Dmaven.javadoc.skip=true -DskipTests'
 
-concurrency:
-  group: ci-${{ github.event.pull_request.number || case(github.repository == 'apache/ratis-thirdparty', github.sha, github.ref_name) }}
-  cancel-in-progress: ${{ github.event_name == 'pull_request' || github.repository != 'apache/ratis-thirdparty' }}
-
-permissions: { }
-
-jobs:
-  CI:
-    if: github.event_name == 'pull_request'
-      || github.repository == 'apache/ratis-thirdparty'
-      || github.ref_name != 'master'
-    uses: ./.github/workflows/reusable-ci.yaml
-    secrets:
-      DEVELOCITY_ACCESS_KEY: ${{ secrets.DEVELOCITY_ACCESS_KEY }}
+source "${DIR}/_mvn_check.sh" install "$@"
